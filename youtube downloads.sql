c41eeb5da68cc55abdfd1a632bfbf4e7aee097e7
Sanjyot525 <sanjyot525@gmail.com>
2021-07-11 20:42:05 +0530

    Add files via upload

---
+++ youtube downloads.sql
@@ -0,0 +1,173 @@
+01 11 2020 BIOSCOPE KI BAATEIN FILM MIRZA GHALIB 1954
+02 08 2020 BIOSCOPE KI BAATEIN FILM PAKEEZAH 1972
+03 01 2021 BIOSCOPE KI BAATEIN FILM AARAM 1951
+04 04 2021 BIOSCOPE KI BAATEIN FILM 36 GHANTE 1974
+04 10 2020 BIOSCOPE KI BAATEIN FILM GUDDI 1971
+05 07 2020 BIOSCOPE KI BAATEIN FILM HAQEEQAT 1964
+06 09 2020 BIOSCOPE KI BAATEIN FILM ANURAAG 1972
+06 12 2020 BIOSCOPE KI BAATEIN FILM AAN 1952
+07 02 2021 BIOSCOPE KI BAATEIN FILM BAWRE NAIN 1950
+07 03 2021 BIOSCOPE KI BAATEIN FILM AN EVENING IN PARIS 1967
+07-06-2020 BIOSCOPE KI BAATEIN FILM SARASWATICHANDRA 1968
+08 11 2020 BIOSCOPE KI BAATEIN FILM ANOKHI RAAT 1968
+09 08 2020 BIOSCOPE KI BAATEIN FILM KABULIWALA 1961
+10 01 2021 BIOSCOPE KI BAATEIN FILM BAHAREN PHIR BHI AAYENGI 1966
+11 04 2021 BIOSCOPE KI BAATEIN FILM AMANUSH 1975
+11 10 2020 BIOSCOPE KI BAATEIN FILM SAHIB BIBI AUR GHULAM 1962
+12 07 2020 BIOSCOPE KI BAATEIN FILM PARAKH 1960
+13 09 2020 BIOSCOPE KI BAATEIN FILM SHOLAY 1975
+13 12 2020 BIOSCOPE KI BAATEIN FILM SURAJ KA SATVAN GHODA 1993
+14 02 2021 BIOSCOPE KI BAATEIN FILM MADHUMATI 1958
+14 03 2021 BIOSCOPE KI BAATEIN FILM AADMI AUR INSAAN 1969 1969
+14 06 2020 BIOSCOPE KI BAATEIN FILM MIRZA GHALIB 1954
+15 11 2020 BIOSCOPE KI BAATEIN FILM PADOSAN 1968
+16 08 2020 BIOSCOPE KI BAATEIN FILM BRAHMACHARI 1968
+17 01 2021 BIOSCOPE KI BAATEIN FILM DEVDAS 1955
+18 10 2020 BIOSCOPE KI BAATEIN FILM JAWAB 1985
+19 07 2020 BIOSCOPE KI BAATEIN FILM ANAND 1971
+20 09 2020 BIOSCOPE KI BAATEIN FILM ITTEFAQ 1969
+20 12 2020 BIOSCOPE KI BAATEIN FILM AAKROSH 1980
+21 02 2021 BIOSCOPE KI BAATEIN FILM KORA KAGAZ 1974
+21 03 2021 BIOSCOPE KI BAATEIN FILM BOMBAY TO GOA 1972
+21 06 2020 BIOSCOPE KI BAATEIN FILM ANUBHAV 1971
+21 07 2020 AAJ KE FANKAR LYRICIST ANAND BAKSHI
+22 11 2020 BIOSCOPE KI BAATEIN FILM DHUND 1973
+23 08 2020 BIOSCOPE KI BAATEIN FILM TEEN BAHURANIYA 1968
+24 01 2021 BIOSCOPE KI BAATEIN FILM UPKAR 1967
+25 10 2020 BIOSCOPE KI BAATEIN FILM SADHNA 1958
+27 09 2020 BIOSCOPE KI BAATEIN FILM ASLI NAQLI 1962
+27 12 2020 BIOSCOPE KI BAATEIN FILM AAN MILO SAJNA 1970
+28 02 2021 BIOSCOPE KI BAATEIN FILM OONCHE LOG 1965
+28 03 2021 BIOSCOPE KI BAATEIN FILM AMAR AKBAR ANTHONY 1977
+28 06 2020 BIOSCOPE KI BAATEIN FILM CARAVAN 1971
+29 11 2020 BIOSCOPE KI BAATEIN FILM SEEMA 1971
+30 08 2020 BIOSCOPE KI BAATEIN FILM SANGAM 1964
+31 01 2021 BIOSCOPE KI BAATEIN FILM JAHAN PYAR MILE 1970
+31-05-2020 BIOSCOPE KI BAATEIN-ANARI 1959
+Ameen Sayani's Geetmala _ Season 40 _ Chupke Chupke Chal Re Purbaiya _ Tum Bhi Chalo Hum Bhi Chalen
+AWS Certified Cloud Practitioner Training 2020 - Full Course
+AWS Tutorial For Beginners in Hindi _ AWS in Hindi _ Edureka Hindi
+Bash Scripting Full Course 3 Hours
+Best of CID (सीआईडी) - DCP's Wedding Invitation - Full Episode
+BIOSCOPE KI BAATEIN FILM AMRAPALI 1966
+BIOSCOPE KI BAATEIN FILM GUMRAH 1963
+BIOSCOPE KI BAATEIN FILM M INDIA 1957
+BIOSCOPE KI BAATEIN FILM MAA AUR MAMTA 1970
+BIOSCOPE KI BAATEIN FILM MUGHAL E AZAM 1960
+BIOSCOPE KI BAATEIN FILM SAFAR 1970
+BIOSCOPE KI BAATEIN FILM SANGAM 1964
+BIOSCOPE KI BAATEIN FILM SHOLAY 1975
+BIOSCOPE KI BAATEIN FILM WAQT 1965
+BIOSCOPE KI BAATEIN FILM-ARADHNA 1969
+BIOSCOPE KI BAATEIN NIRMAN KI KAHANI BY LOKENDRA SHARMA
+CHANGE MANAGEMENT Interview Questions And Answers! (Leading Change Interview Tips!)
+CI CD Pipeline in Hindi _ Creating a CI CD Pipeline from Scratch _ DevOps Training _ Edureka Hindi
+CID - Choro Ka Khooni Kaun - Episode 1068 - 26th April 2014
+CID - सी ई डी - Khooni Chasma - Episode 1204 - 15th March 2015
+CID _ सीआईडी _ Ep 970 _  The Farm House Mystery _ Full Episode
+Colors in Japanese _ JLPT Vocabulary _Color names and Kanji in Japanese _ Beginners vocabulary
+Day 1 JLPT N3 Kanji _ Learn 336 JLPT N3 Kanjis in 6 Weeks _ study JLPT N3 Kanji in Daily lessons
+Day 2 JLPT N3 Kanji _ Learn 336 JLPT N3 Kanjis in 6 Weeks _ study JLPT N3 Kanji in Daily lessons
+Day 3 JLPT N3 Kanji _ Learn 336 JLPT N3 Kanjis in 6 Weeks _ study JLPT N3 Kanji in Daily lessons
+Day 4 JLPT N3 Kanji _ Learn 336 JLPT N3 Kanjis in 6 Weeks _ study JLPT N3 Kanji in Daily lessons
+Day 5 JLPT N3 Kanji _ Learn 336 JLPT N3 Kanjis in 6 Weeks _ study JLPT N3 Kanji in Daily lessons
+Day 6 JLPT N3 Kanji _ Learn 336 JLPT N3 Kanjis in 6 Weeks _ study JLPT N3 Kanji in Daily lessons
+Day 7 JLPT N3 Kanji _ Learn 336 JLPT N3 Kanjis in 6 Weeks _ study JLPT N3 Kanji in Daily lessons
+DevOps Interview Questions and Answers _ DevOps Jobs _ DevOps Engineer _ DevOps Training _ Edureka
+DevOps Tutorial For Beginners in Hindi _ What is DevOps in Hindi _ DevOps Training _ Edureka
+Docker Architecture _ Components
+Docker for Beginnners
+Docker Installation On Ubuntu_ _ Linux
+Docker Installation on Windows
+Docker Pull _ Delete _ Ubuntu Image
+Docker Tutorial in Hindi
+Financial Analytics with Python in Hindi _ How to Build Financial Models with Python _Great Learning
+Fruits in Japanese _ JLPT Vocabulary _ Fruits names and Kanji in Japanese _ Beginners vocabulary
+Household Words in Japanese _ JLPT Vocabulary _ house and furniture words in Japanese _ JLPT words
+How To Become a SQL Developer _ SQL for Beginners _ SQL Developer Roadmap _ Great Learning
+Interview Question_ Tell me about a time you handled a difficult situation
+IT Service Management Tutorial _ What Is ITSM_ _ ITIL Foundation Training _ Simplilearn
+ITIL 4 Foundation Full Course _ ITIL Tutorial For Beginners _ITIL Certification Training_Simplilearn
+ITIL 4 interview with Akshay Anand
+ITIL Interview Questions And Answers _ ITIL Foundation Certification Training _ Simplilearn
+ITIL Interview Questions and Answers _ ITIL® Foundation _
+ITIL Processes Explained _ ITIL v3 Framework _  ITIL® Foundation Training _ Edureka
+ITIL v3 vs ITIL 4 _ Difference between ITIL v3 and ITIL 4 _ ITIL® Foundation Training _ Edureka
+Jenkins Full Course _ Jenkins Tutorial For Beginners _ Jenkins Tutorial _ Simplilearn
+Jenkins Installation In Windows _ How To Install Jenkins On Windows 10 _ Simplilearn
+Jenkins Interview Questions _ Jenkins Interview Questions And Answers _ DevOps Tools _ Simplilearn
+Jenkins Pipeline Tutorial _ CI CD Pipeline using Jenkins _ Jenkins Tutorial _ Simplilearn
+Jenkins Tutorial _ Jenkins Installation _ Continuous Integration With Jenkins _ DevOps _ Simplilearn
+Jenkins Tutorial For Beginners _ Jenkins Tutorial _ Jenkins Continuous Integration _ Simplilearn
+Jenkins Tutorial For Beginners _ What Is Jenkins _ DevOps Tutorial _ DevOps Tools _ Simplilearn
+Jenkins Tutorial For Beginners In Hindi _ DevOps For Beginners _ DevOps Tools _ Great Learning
+JLPT N2 Grammar さびしげ
+JLPT N3 Grammar Questions and Answers _ Sample JLPT Questions and Answers _ JLPT N3 Past Questions
+JLPT N3 Grammar Questions and Answers _ Sample JLPT Questions and Answers _ Model Test _ Japanese
+JLPT N3 Kanji Quiz [20 Multiple Choice Questions with Answers] _ JLPT N3 Kanji _ JLPT N3 Vocabulary
+JLPT N3 Listening Questions and Answers _ N3 JLPT Questions _ sample JLPT N3 Past Question _ 聴解
+JLPT N3 Vocabulary Questions and Answers _ Sample JLPT Questions and Answers _ JLPT N3 Exercise
+JLPT N3 Vocabulary Questions and Answers _ Sample JLPT Questions and Answers _ JLPT N3 Past question
+Job Interview Skills in 2020 _ Best Interview Tips to crack your Interviews _ Edureka
+Learn in 60secs! - Difference between AI, Machine Learning (ML) and Deep Learning _ Great Learning
+Linux Interview Questions And Answers _ Linux Administration Tutorial _ Linux Training _ Edureka
+Linux Tutorial For Beginners - 1 _ Linux Administration Tutorial _ Linux Commands _ Edureka
+Machine Learning Model Deployment Using Heroku _ Heroku Tutorial for Beginners _ Great Learning
+Must learn Japanese Vocabulary and Example Sentence _ JLPT Vocabulary _ JLPT Words _ JLPT ことば
+MySQL Tutorial for Beginners [Full Course]
+New folder
+No valid account found!1Xfs4RfmEw7Sv5XjHPo7Vwr1dFOxUW-CK
+OpenCV Course - Full Tutorial with Python
+Production Issue!!!
+Python for Data Analysis _ Python for Data Visualisation _ Python Tutorial _ Learn Python
+Radio Ceylon 13 04 2021 Tuesday Morning
+ServiceNow Interview Questions and Answers _ Most Asked Interview Questions _
+Shell Scripting Interview Questions & Answers _ Linux Admin Certification Training _ Edureka
+Shell Scripting Tutorial _ Shell Scripting Crash Course _ Linux Certification Training _ Edureka
+Similar Looking Kanji in Japanese _ Simple Kanji in Japanese _ Similar Looking Kanji _ 漢字 _ KANJI
+SQL Interview Questions _ SQL Interview tips & tricks _ Great Learning
+SQL Tutorial For Beginners _ SQL Full Course _ Learn SQL in 4 Hours _ Great Learning
+SQL Tutorial For Beginners In Hindi _ DBMS Tutorial _ SQL Full Course In Hindi _ Great Learning
+Top 50 ITIL Interview Questions and Answers _ ITIL® Foundation Training _ Edureka
+Top 65 SQL Interview Questions and Answers _ SQL Interview Preparation _ SQL Training _ Edureka
+Top Christmas words in Japanese _ Japanese Christmas vocabulary _ Daily life Japanese words _　日本語言葉
+What is Docker and how it works _ _ Docker tutorial for Beginners _ Microservices _ Devops Tools
+What is Docker_
+What is Incident Management in ServiceNow _ ServiceNow Incident Management Process
+What is ITIL® v4_ ITIL® Certification Explained _  ITIL® Foundation Training _ Edureka
+What is Jenkins in Hindi _ Jenkins Tutorial for Beginners in Hindi _ DevOps Training _ Edureka Hindi
+What Is Jenkins_ _ What Is Jenkins And How It Works_ _ Jenkins Tutorial For Beginners _ Simplilearn
+What is LDAP and Active Directory _ How LDAP works and what is the structure of LDAP_AD_
+What is Problem Management in ServiceNow _  ServiceNow Problem Management Process
+चार्वी _पानिपत (मर्द मराठा )
+日本語教室N3（日本語総まとめN3文法　第1週1日目③）
+日本語教室N3（日本語総まとめN3文法　第1週2日目）
+日本語教室N3（日本語総まとめN3文法　第1週3日目）
+日本語教室N3（日本語総まとめN3文法　第1週6日目）
+日本語教室N3（日本語総まとめN3文法　第2週3日目）
+日本語教室N3（日本語総まとめN3文法　第2週4日目）
+日本語教室N3（日本語総まとめN3文法　第2週5日目）
+日本語教室N3（日本語総まとめN3文法　第3週1日目）
+日本語教室N3（日本語総まとめN3文法　第3週2日目）
+日本語教室N3（日本語総まとめN3文法　第3週3日目）
+日本語教室N3（日本語総まとめN3文法　第3週4日目）
+日本語教室N3（日本語総まとめN3文法　第3週5日目）
+日本語教室N3（日本語総まとめN3文法　第3週6日目）
+日本語教室N3（日本語総まとめN3文法　第4週1日目）
+日本語教室N3（日本語総まとめN3文法　第4週2日目）
+日本語教室N3（日本語総まとめN3文法　第4週3日目）
+'ती' च्या अवकाशात डोकावताना  भाग ८  _  सुजाता कोळेकर  _ (Description).txt
+Creating Django App on Docker (Description).txt
+Docker Architecture _ Components (Description).txt
+Docker Installation on Windows (Description).txt
+Dockerize Django App (Description).txt
+Linux Interview Questions And Answers _ Linux Administration Tutorial _ Linux Training _ Edureka (Description).txt
+What is Docker_ (Description).txt
+日本語教室N3（日本語総まとめN3文法　第1週1日目①） (Description).txt
+日本語教室N3（日本語総まとめN3文法　第1週1日目②） (Description).txt
+日本語教室N3（日本語総まとめN3文法　第1週4日目） (Description).txt
+日本語教室N3（日本語総まとめN3文法　第1週5日目） (Description).txt
+日本語教室N3（日本語総まとめN3文法　第2週1日目） (Description).txt
+日本語教室N3（日本語総まとめN3文法　第2週2日目） (Description).txt
+日本語教室N3（日本語総まとめN3文法　第2週5日目） (Description).txt
+日本語教室N3（日本語総まとめN3文法　第2週6日目） (Description).txt
